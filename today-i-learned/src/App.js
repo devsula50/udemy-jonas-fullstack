03e2440b0420fed33d22bb00ccc8b5773b7f186f
devsula50 <devsula50@gmail.com>
2023-09-20 13:47:39 +0900

    # Rendering the List of Categories
    1. CategoryFilter로 aside 내용 이동
    2. CATEGORIES에 map 돌려서 categories 뿌리

--- today-i-learned/src/App.js
+++ today-i-learned/src/App.js
@@ -79,7 +79,27 @@ const NewFactForm = () => {
 }
 
 const CategoryFilter = () => {
-  return <aside>Category filter</aside>
+  return (
+    <aside>
+      <ul>
+        <li>
+          <button className="btn btn-all-categories">All</button>
+        </li>
+        {
+          CATEGORIES.map(cat =>
+            <li key={cat.name} className="category">
+              <button
+                className="btn btn-category"
+                style={{ backgroundColor: cat.color }}
+              >
+                {cat.name}
+              </button>
+            </li>
+          )
+        }
+      </ul>
+    </aside>
+  )
 }
 
 const FactList = () => {
